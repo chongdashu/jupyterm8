843765ee2c1c26954e9f92f7f043487b8586dfea
Chong-U Lim <me@chongdashu.com>
2024-08-30 15:33:44 +0100

    Update layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,41 @@
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Jupyter Notebook Parser",
-  description: "Parse and download Jupyter notebooks",
+export const metadata: Metadata = {
+  title: "Jupyterm8 - Jupyter Notebook Tools",
+  description:
+    "Suite of tools for data scientists, developers, and researchers working with Jupyter Notebooks. Extract, convert, and manage your notebooks efficiently.",
+  keywords:
+    "Jupyter Notebook, extractor, data science, notebook converter, code search, version control",
+  // openGraph: {
+  //   title: "Jupyterm8 - Jupyter Notebook Tools",
+  //   description:
+  //     "Streamline your Jupyter Notebook workflow with our suite of tools, including our Notebook Extractor.",
+  //   url: "https://www.jupyterm8.xyz",
+  //   siteName: "Jupyterm8",
+  //   images: [
+  //     {
+  //       url: "https://www.jupyterm8.xyz/og-image.jpg",
+  //       width: 1200,
+  //       height: 630,
+  //       alt: "Jupyterm8 - Jupyter Notebook Tools",
+  //     },
+  //   ],
+  //   locale: "en_US",
+  //   type: "website",
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "Jupyterm8 - Jupyter Notebook Tools",
+  //   description:
+  //     "Streamline your Jupyter Notebook workflow with our suite of tools, including our Notebook Extractor.",
+  //   images: ["https://www.jupyterm8.xyz/og-image.jpg"],
+  // },
+  robots: "index, follow",
+  // themeColor: "#ffffff",
 };
 
 export default function RootLayout({
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,9 @@ export default function Header() {
   return (
     <header className="bg-gradient-to-r from-blue-600 to-indigo-600 text-white py-4 shadow-md">
       <div className="container mx-auto px-4">
-        <h1 className="text-2xl font-bold">Notebook Parser</h1>
+        <h1 className="text-2xl font-bold">
+          Jupyterm8 - Tools for Jupyter Notebooks
+        </h1>
       </div>
     </header>
   );
